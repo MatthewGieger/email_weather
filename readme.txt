53f74ce9efc01543db5f8ba9970584f1224cb828
matthewgieger <mattbju2013@gmail.com>
2017-03-30 22:53:48 -0400

    updated readme

--- readme.txt
+++ readme.txt
@@ -2,6 +2,14 @@ AUTHOR:
   Matthew Gieger
 
 DESCRIPTION:
-  A simply python program that fetches weather data and emails it out.
+  A simple python program that fetches weather data from WeatherUnderground and emails it out.
 
+REQUIREMENTS:
+  Python 2
+  Weather Underground API Key (can be created for free here: https://www.wunderground.com/weather/api/)
+
+INSTALLATION/CONFIGURATION:
+  1.  Create a settings.py file in the roor project directory.
+  2.  Edit settings.py to your liking using settings_example.py as a template or reference. 
+  3.  Run "python main.py" from the terminal.
 
--- settings_example.py
+++ settings_example.py
@@ -1,8 +1,9 @@
-# Change zip code in the below url to desired location
-weather_url = "http://api.wunderground.com/api/2c8883d85ca5d155/forecast/q/12748.json"
-from_email = "sending_email@gmail.com"
-from_email_password = "password_here"
-to_email = "receiving_email@gmail.com"
+weather_url = "http://api.wunderground.com/api/{key}/forecast/q/{zip}.json".format(
+        key='weather_underground_api_key',  # get key: wunderground.com/weather/api/
+        zip='13778')
+from_email = "sending_email@gmail.com"  # The email you want to send from
+from_email_password = "password_here"  # The sending email's password
+to_email = "receiving_email@gmail.com"  # The email receiving the weather
 smtp_server = "smtp.gmail.com"  # Change if you're not using gmail
 smtp_port = 587  # May need to change if you're not using gmail
 forecast_days = (0, 1)  # Days to get weather (0 = today, 1 = tomorrow, etc.)
